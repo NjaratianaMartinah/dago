b24b8196e72ec7e27706c6ff7e47d1881c92f46b
Njaratiana <njaratiana.rahalinjanahary@pulse.mg>
2023-01-13 08:59:36 +0300

    init project

---
+++ src/main/java/com/example/dago/DagoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dago/DagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
