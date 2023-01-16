e7a072a424965727edfb77e06521c0b69f08b0bb
Njaratiana <njaratiana.rahalinjanahary@pulse.mg>
2023-01-16 15:22:46 +0300

    create payment model

---
+++ src/main/java/com/example/dago/models/Payment.java
@@ -0,0 +1,40 @@
+package com.example.dago.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP")
+    private Date date;
+
+    @Column(name="amount", nullable=false, columnDefinition = "DOUBLE NOT NULL DEFAULT 0")
+    private Double amount;
+   
+    @Column(name="start_counter", nullable=false, columnDefinition = " INT NOT NULL DEFAULT 0")
+    private int startCounter;
+
+    @Column(name="end_counter", nullable=false, columnDefinition = " INT NOT NULL DEFAULT 0")
+    private int endCounter;
+
+    @Column(name="diff_counter", nullable=false, columnDefinition = " INT NOT NULL DEFAULT 0")
+    private int diffCounter;
+
+    @Column(name="responsible", nullable=false, length = 200)
+    private String responsible;
+    
+}
